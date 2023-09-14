efd727c642b10c6d0959c4766ff075e57ae039a6
Настя Николаева <anastasia_nikolaeva2002@mail.ru>
2023-09-14 23:38:39 +0300

    добавили 10 задачу

---
+++ task10/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+using System;
+
+namespace task10
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+          Console.Write("Введите число: ");
+          int number = int.Parse(Console.ReadLine()!);
+          SecondDigit(number);
+
+        }
+
+        static void SecondDigit(int number){
+
+            int second_digit = (number/10)%10;
+            Console.WriteLine("Вторая цифра = " + second_digit);
+        }
+    }
+}
